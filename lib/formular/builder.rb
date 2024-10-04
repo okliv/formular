fcc76b8100ddc2190ede829b118657a5cd515a48
okliv <884634+okliv@users.noreply.github.com>
2024-10-05 02:01:14 +0300

    pupupu

--- lib/formular/builder.rb
+++ lib/formular/builder.rb
@@ -9,7 +9,7 @@ module Formular
 
     def self.element_set(**elements)
       self.elements.merge!(elements)
-      define_element_methods(**self.elements)
+      define_element_methods(self.elements)
     end
 
     def self.define_element_methods(**elements)
@@ -17,15 +17,15 @@ module Formular
     end
 
     def self.define_element_method(element_name, element_class)
-      define_method(element_name) do |*args, &block|
-        if args.size > 1
-          name, options = args
-        else
-          case args.first
-          when Symbol then name = args.first
-          when Hash then options = args.first
-          end
-        end
+      define_method(element_name) do |*name, **options, &block|
+        # if args.size > 1
+        # #   name, options = args
+        # else
+        #   case args.first
+        #   when Symbol then name = args.first
+        #   when Hash then options = args.first
+        #   end
+        # end
 
         options ||= {}
         options[:builder] = self
